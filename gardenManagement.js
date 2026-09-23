aed726baf581962410e8e0ce7ac922ff49f519e0
Joelmaina <joelkaranja759@gmail.com>
2026-09-23 19:18:16 +0300

    created statements

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,30 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+//watering control
+
+if (temperature > 80) {
+   console.log("Watering on"); 
+}
+else{
+    console.log("Watering off");
+}
+
+//Garden lights control
+
+if (timeOfDay === "morning" || timeOfDay === "night") {
+    console.log("lights on");
+}
+else{
+    console.log("lights off");
+}
+
+//soil moisture adjustment
+
+while (soilMoisture < 40) {
+    soilMoisture +=5;
+    console.log(soilMoisture);
+}
+
+
